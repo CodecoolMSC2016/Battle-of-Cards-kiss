850e59216885e47fd610039727b59ddb69c4d63d
lugos <lugos@DESKTOP-IS3NSR2>
2017-02-28 11:58:20 +0100

    Beta version is done!

--- src/cardgame/Main.java
+++ src/cardgame/Main.java
@@ -2,15 +2,11 @@ package cardgame;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
-		Dealer dealer = new Dealer(player1, player2);
-		dealer.deal();
-		
-		System.out.println(player1.getHandOfPlayer());
-		
-		System.out.println(player2.getHandOfPlayer());
+		Round round = new Round(player1, player2);
+		round.round();
+		System.out.format("Player 1 Score: %s %n" ,round.getCounterPlayer1());
+		System.out.format("Player 2 Score: %s" ,round.getCounterPlayer2());
 	}
 }
--- src/cardgame/Round.java
+++ src/cardgame/Round.java
@@ -1,5 +1,55 @@
 package cardgame;
 
-public class Round {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Round extends Dealer {
+	public Round(Player player1, Player player2)
+	{
+		super(player1, player2);
+	}
+
+	private int counterPlayer1 = 0;
+	private int counterPlayer2 = 0;
+	
+	public int getCounterPlayer1()
+	{
+		return counterPlayer1;
+	}
+
+	public void setCounterPlayer1(int number)
+	{
+		this.counterPlayer1 += number;
+	}
+
+	public int getCounterPlayer2()
+	{
+		return counterPlayer2;
+	}
+	public void setCounterPlayer2(int number)
+	{
+		this.counterPlayer2+= number;
+	}
+
+	public void round()
+	{
+		Dealer dealer = new Dealer(player1, player2);
+		dealer.deal(); 
+		for(int i = 0; i < player2.getHandOfPlayer().size(); i++)
+		{	
+			if (player1.getHandOfPlayer().get(i).tSpeed == player2.getHandOfPlayer().get(i).tSpeed)
+			{
+				continue;
+			}
+			if (player1.getHandOfPlayer().get(i).tSpeed > player2.getHandOfPlayer().get(i).tSpeed)
+			{
+				setCounterPlayer1(1);
+			}
+			else
+			{
+				setCounterPlayer2(1);
+			}
+			
+		}
+	}	
 }
