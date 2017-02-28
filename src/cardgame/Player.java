16ccb53909c8946aa50fa870083543a9ae4fcc0f
agnes-kabaly <alicet.woods@gmail.com>
2017-02-28 09:18:56 +0100

    correction

--- src/cardgame/Player.java
+++ src/cardgame/Player.java
@@ -7,17 +7,9 @@ public class Player {
 
 	List<Cars> handOfPlayer = new ArrayList<Cars>();
 
-	public Player(List<Cars> handOfPlayer) {
-		this.handOfPlayer = handOfPlayer;
-	}
-	
-	public Player() {
-	}
-	
 	public void addCard(Cars car) {
 		handOfPlayer.add(car);
 	}
-	// Enum<?> e
 
 	public List<Cars> getHandOfPlayer() {
 		return handOfPlayer;
