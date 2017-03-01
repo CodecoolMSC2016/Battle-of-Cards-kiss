8f3ffd737d78974b90ccd0b381e07e58416ea04e
agnes-kabaly <alicet.woods@gmail.com>
2017-03-01 13:34:18 +0100

    NextCard showing

--- src/cardgame/Player.java
+++ src/cardgame/Player.java
@@ -22,4 +22,8 @@ public class Player {
 	public List<Cars> getHandOfPlayer() {
 		return handOfPlayer;
 	}
+
+	public void showCard(int index) {
+		System.out.println(handOfPlayer.get(index));
+	}
 }
--- src/cardgame/Round.java
+++ src/cardgame/Round.java
@@ -8,6 +8,7 @@ public class Round extends Dealer {
 		comp = new CardComparator();
 
 	}
+
 	public void round() {
 		Dealer dealer = new Dealer(player1, player2);
 		dealer.deal();
@@ -27,9 +28,11 @@ public class Round extends Dealer {
 			if (tempComp > 0) {
 				player1.setScore(1);
 				System.out.println("Round won by Player1 \n");
+				player1.showCard(i + 1);
 			} else {
 				player2.setScore(1);
 				System.out.println("Round won by Player2 \n");
+				player2.showCard(i + 1);
 			}
 
 		}
