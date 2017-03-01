604fe9e808054ba79937bb142405147f1859ef0e
lugos <lugos@DESKTOP-IS3NSR2>
2017-03-01 13:52:28 +0100

    Game simplification improvements

--- src/cardgame/CardComparator.java
+++ src/cardgame/CardComparator.java
@@ -7,33 +7,38 @@ public class CardComparator implements Comparator<Cars> {
 
 	@Override
 	public int compare(Cars player1Card, Cars player2Card) {
-		String[] good = {"TopSpeed", "Weight", "Displacement", "Cylinder", "Acceleration"};
+		String[] good = {"S", "W", "D", "C", "A", "s", "w", "d", "c", "a"};
 		String finalOption = "";
 		while (!Arrays.asList(good).contains(finalOption))
 		{
 			Scanner scan = new Scanner(System.in);
 			System.out.println("Please Chose an option from the followings");
-			System.out.println("TopSpeed, Weight, Displacement, Cylinder, Acceleration");
+			System.out.println(
+							  "S = TopSpeed,"
+							+ "W = Weight,"
+							+ "D = Displacement,"
+							+ "C = Cylinder,"
+							+ "A = Acceleration");
 			String option = scan.nextLine();
-			finalOption = option;
+			finalOption = option.toLowerCase();
 		}
-		if (finalOption.equals("TopSpeed"))
+		if (finalOption.equals("s"))
 		{
 			return player1Card.tSpeed - player2Card.tSpeed;
 		}
-		else if (finalOption.equals("Weight"))
+		else if (finalOption.equals("w"))
 		{
 			return player2Card.weight - player1Card.weight;
 		}
-		else if (finalOption.equals("Displacement"))
+		else if (finalOption.equals("d"))
 		{
 			return player1Card.displacement - player2Card.displacement;
 		}
-		else if (finalOption.equals("Cylinder"))
+		else if (finalOption.equals("c"))
 		{
 			return player1Card.cylinder - player2Card.cylinder;
 		}
-		else if (finalOption.equals("Acceleration"))
+		else if (finalOption.equals("a"))
 		{
 			int returner = (int)Math.round((player2Card.acceleration - player1Card.acceleration));
 			return returner;
