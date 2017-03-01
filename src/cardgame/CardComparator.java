bf014c91567d27d45a74c465cac1adc4b4fdb2dc
lugos <lugos@DESKTOP-IS3NSR2>
2017-03-01 11:45:33 +0100

    Comparator inprovement halfly done!

--- src/cardgame/CardComparator.java
+++ src/cardgame/CardComparator.java
@@ -1,4 +1,5 @@
 package cardgame;
+import java.util.Scanner; 
 
 import java.util.Comparator;
 
@@ -6,7 +7,30 @@ public class CardComparator implements Comparator<Cars> {
 
 	@Override
 	public int compare(Cars player1Card, Cars player2Card) {
-
-		return player1Card.tSpeed - player2Card.tSpeed;
+		String[] good = {"TopSpeed", "Weight", "Displacement", "Cylinder", "Acceleration"};
+		boolean goodInput = false;
+		String finalOption = "";
+		while (goodInput == false)
+		{
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please Chose an option from the followings");
+			System.out.println("TopSpeed, Weight, Displacement, Cylinder, Acceleration");
+			String option = scan.nextLine();
+			if (good.)
+			{
+				
+			}
+		}
+		if (finalOption.equals("TopSpeed"))
+		{
+			return player1Card.tSpeed - player2Card.tSpeed;
+		}
+		else if (finalOption.equals("Weight"))
+		{
+			return player2Card.weight - player1Card.weight;
+		}
+		return 0;
+		
+		
 	}
 }
