d7b26e8de42016bfdee2835493cf9d97427097a3
Jakubinyi <jakubinyi@gmail.com>
2017-03-01 15:45:52 +0100

    add methods: compareCards, createComparatorMap

--- src/cardgame/Game.java
+++ src/cardgame/Game.java
@@ -26,15 +26,69 @@ public class Game  {
 	public void playGame() {
 		Dealer dealer = new Dealer(player1, player2);
 		dealer.deal();
-		int tempComp;
 		
+		createComparatorMap();
+		
+		System.out.println("QUARTETT GAME: \n");
+		
+		selectPlayer();		
+		readInput();
+		compareCards();
+	}
+	
+	public void createComparatorMap() {
 		mComparators.put("s", new SpeedComparator());
 		mComparators.put("w", new WeightComparator());
 		mComparators.put("d", new DisplacementComparator());
 		mComparators.put("c", new CylinderComparator());
 		mComparators.put("a", new AccelerationComparator());
+	}
+	
+	public void selectPlayer() {
+		int input;
+		boolean validInput = false;
 		
-		readInput();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Which player would like to start the game? [1/2]");
+		input = scan.nextInt();
+		
+		while(validInput == false)
+		if(input == 1) {
+			System.out.println("Player1 card: " + player1.getHandOfPlayer().get(0));
+			validInput = true;
+		} else if (input == 2) {
+			System.out.println("Player2 card: " + player2.getHandOfPlayer().get(0));
+			validInput = true;
+		} else {
+			System.out.println("Not valid input! \n");
+			System.out.println("Which player would like to start the game? [1/2]");
+			input = scan.nextInt();
+		}
+		
+	}
+	
+	public void readInput() {
+		String input = "";
+		String[] good = {"S", "W", "D", "C", "A", "s", "w", "d", "c", "a"};
+		
+		while (!Arrays.asList(good).contains(input)) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please Chose an option from the followings:");
+			System.out.println(
+							  "S = TopSpeed,"
+							+ "W = Weight,"
+							+ "D = Displacement,"
+							+ "C = Cylinder,"
+							+ "A = Acceleration");
+			String option = scan.nextLine();
+			input = option.toLowerCase();
+		}
+		
+		comparator = mComparators.get(input);
+	}
+	
+	public void compareCards() {
+		int tempComp;
 		
 		for (int i = 0; i < player2.getHandOfPlayer().size(); i++) {
 			System.out.println();
@@ -63,26 +117,6 @@ public class Game  {
 			}
 			readInput();
 		}
-	}
-	
-	public void readInput() {
-		String input = "";
-		String[] good = {"S", "W", "D", "C", "A", "s", "w", "d", "c", "a"};
-		
-		while (!Arrays.asList(good).contains(input)) {
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Please Chose an option from the followings");
-			System.out.println(
-							  "S = TopSpeed,"
-							+ "W = Weight,"
-							+ "D = Displacement,"
-							+ "C = Cylinder,"
-							+ "A = Acceleration");
-			String option = scan.nextLine();
-			input = option.toLowerCase();
-		}
-		
-		comparator = mComparators.get(input);
 	}
 	
 	public void endGame(Player player1, Player player2) {
